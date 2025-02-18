6347e67f612854eb19df62f06c11671287e48612
Andyrewy7 <106855896+AndrewY7@users.noreply.github.com>
2025-02-18 17:42:10 -0500

    finished simple calculations

--- Grades/GradeCalculator.java
+++ Grades/GradeCalculator.java
@@ -13,9 +13,28 @@ public class GradeCalculator {
         grades.add(course);
     }
 
-    public double Calculate(List<Course> grades) {
+    public double CalculateGrade(List<Course> grades) {
         this.grades = grades;
 
-        return grades[0] + 10;
+        double totalWeightedGrade = 0;
+        double totalWeight = 0;
+
+        for (Course course : grades) {
+            totalWeightedGrade += course.getWeightedGrade();
+            totalWeight += course.getCourseWeight();
+        }
+
+        if (totalWeight == 0) {
+            return 0;
+        }
+
+        return totalWeightedGrade / totalWeight;
+    }
+
+    public double CalculateFinalExamGrade(double desiredGrade, double finalWeight, List<Course> grades) {
+        double currentGrade = CalculateGrade(grades);
+        double currentWeight = 100 - finalWeight;
+
+        return (desiredGrade - (currentGrade * (currentWeight / 100))) / (finalWeight / 100);
     }
 }
